07ede04307a02dea28e90f1cf61ea105878f5a4e
HOME-PC <demanin@bk.ru>
2024-05-22 19:38:19 +0300

    ED9IH

---
+++ order-service/src/main/java/ru/demanin/entity/RestaurantMenuItems.java
@@ -0,0 +1,32 @@
+package ru.demanin.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "restaurant_menu_items")
+public class RestaurantMenuItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private long price;
+    @Column(name = "image")
+    private String image;
+    @Column(name = "description")
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id",referencedColumnName = "id")
+    private Restaurant restaurant;
+}
