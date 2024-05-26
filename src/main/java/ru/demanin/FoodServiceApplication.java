96aa7e266c6c3b2755c50d2ccec2fb689d322903
HOME-PC <demanin@bk.ru>
2024-05-26 20:45:40 +0300

    ED9IH

--- src/main/java/ru/demanin/FoodServiceApplication.java
+++ src/main/java/ru/demanin/FoodServiceApplication.java
@@ -3,11 +3,10 @@ package ru.demanin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
-public class OrderServiceApplication {
+public class FoodServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class,args);
+        SpringApplication.run(FoodServiceApplication.class,args);
+
     }
 }
